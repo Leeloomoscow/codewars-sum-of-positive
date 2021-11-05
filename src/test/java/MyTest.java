7099d99390e72c98b71b8c85292f55b9a9e4d5ca
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-11-05 19:28:15 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,25 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void testSomething() {
+        assertEquals(15, Positive.sum(new int[]{1, 2, 3, 4, 5}));
+        assertEquals(13, Positive.sum(new int[]{1, -2, 3, 4, 5}));
+        assertEquals(0, Positive.sum(new int[]{}));
+        assertEquals(0, Positive.sum(new int[]{-1, -2, -3, -4, -5}));
+        assertEquals(9, Positive.sum(new int[]{-1, 2, 3, 4, -5}));
 
+    }
+
+    private static class Positive {
+        public static int sum(int[] arr) {
+            int sum = 0;
+            for (int i : arr) {
+                if (i > 0) sum += i;
+            }
+            return sum;
+        }
     }
 }
